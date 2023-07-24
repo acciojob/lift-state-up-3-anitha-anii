a1a475d340c76b586f370015ebd0a190e4941f00
anitha-anii <anithachowdary355@gmail.com>
2023-07-24 17:14:34 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+// Child Component 1
+class ChildComponent1 extends Component {
+  render() {
+    return (
+      <button onClick={() => this.props.onOptionSelect("Option 1")}>
+        Button 1
+      </button>
+    );
+  }
 }
 
-export default App
+// Child Component 2
+class ChildComponent2 extends Component {
+  render() {
+    return (
+      <button onClick={() => this.props.onOptionSelect("Option 2")}>
+        Button 2
+      </button>
+    );
+  }
+}
+
+// Parent Component
+class ParentComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: null,
+    };
+  }
+
+  handleOptionSelect = (option) => {
+    this.setState({ selectedOption: option });
+  };
+
+  render() {
+    return (
+      <div className="parent">
+        <ChildComponent1 onOptionSelect={this.handleOptionSelect} />
+        <ChildComponent2 onOptionSelect={this.handleOptionSelect} />
+        <p>Selected Option: {this.state.selectedOption}</p>
+      </div>
+    );
+  }
+}
+
+export default ParentComponent;
